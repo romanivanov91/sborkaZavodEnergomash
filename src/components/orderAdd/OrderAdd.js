7d4b064c845bd186eea431ea663967f1575c770c
Ivanov Roman <romanivanov91@yandex.ru>
2023-07-26 07:36:16 +0300

    three

--- src/components/orderAdd/OrderAdd.js
+++ src/components/orderAdd/OrderAdd.js
@@ -3,7 +3,102 @@ import './OrderAdd.css';
 const OrderAdd = () => {
     return(
         <div className = 'orderAdd'>
-        <h1>Это форма добавления заказа</h1>
+            <h1>Добавить заказ</h1>
+            <form className='orderAdd_form'>
+                <div className='orderAdd_form_order'>
+                    <div>
+                        <label>Год</label>
+                        <input
+                            required
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Заказчик</label>
+                        <input
+                            required
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Дата запуска</label>
+                        <input
+                            required
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Дата отгрузки</label>
+                        <input
+                            required
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Ответстсвенный</label>
+                        <input
+                            required
+                            type="text"
+                        ></input>
+                    </div>
+                </div>
+                <div className = 'orderAdd_form_order_products'>
+                    <h3>Продукция</h3>
+                    <div className='orderAdd_form_order_productAdd'>
+                        <div>
+                            <label>Наименование</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Колличество</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Конструктор</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Снабжение отв.</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Монтаж шкафов</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Бригада</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Отгрузка</label>
+                            <input
+                            required
+                            type="text"
+                            ></input>
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </form>
         </div>
     )
 };
