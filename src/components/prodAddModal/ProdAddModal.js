0033ba6e4da18382d61c2d53c948237bcfcf6891
Ivanov Roman <romanivanov91@yandex.ru>
2023-08-02 15:37:32 +0300

    seven

--- src/components/prodAddModal/ProdAddModal.js
+++ src/components/prodAddModal/ProdAddModal.js
@@ -6,42 +6,45 @@ const ProdAddModal = () => {
     
     return (
         <div className='prodAddModal'>
-            <form>
-                <div>
-                    <label>Наименование</label>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <label>Колличество</label>
-                    <input type='number'></input>
-                </div>
-                <div>
-                    <label>Конструктор</label>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <label>Снабжение отв.</label>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <label>Монтаж шкафов</label>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <label>Бригада</label>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <label>Отгрузка</label>
-                    <input type='text'></input>
-                </div>
-                <div>
-                    <input 
-                        type='submit'
-                        value='Добавить продукцию'>
-                    </input>
-                </div>
-            </form>
+           <div className='prodAddModal_body'>
+                <form>
+                    <div>
+                        <label>Наименование</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Колличество</label>
+                        <input type='number'></input>
+                    </div>
+                    <div>
+                        <label>Конструктор</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Снабжение отв.</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Монтаж шкафов</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Бригада</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Отгрузка</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <input 
+                            type='submit'
+                            value='Добавить продукцию'>
+                        </input>
+                    </div>
+                </form>
+                <input className='close_modal' type='button' value='Закрыть'></input>
+           </div>
         </div>
     )
 };
