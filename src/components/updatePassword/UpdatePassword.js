34512631972ea1d6145aff763c3b695e8daabcde
Ivanov Roman <romanivanov91@yandex.ru>
2023-11-22 16:38:21 +0300

    41

--- src/components/updatePassword/UpdatePassword.js
+++ src/components/updatePassword/UpdatePassword.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import { Formik, Form, useField } from 'formik';
 import * as Yup from 'yup';
 import { useHttp } from "../../hooks/http.hook";
 import BarLoader from "react-spinners/BarLoader";
+import { updateUserPass } from "../../actions/index";
+
+import './UpdatePassword.css';
 
 const MyTextInput = ({label, ...props}) => {
     const [field, meta] = useField(props);
@@ -23,25 +27,32 @@ const UpdatePassword = () => {
     const [errorUpdatePassword, setErrorUpdatePassword] = useState(false);
     const [succesUpdatePasswordMesageState, setSuccesUpdatePasswordMesageState] = useState(false);
 
+    const updateUserPassFormState = useSelector(state => state.updateUserPassFormState);
+
+    const dispatch = useDispatch();
+
     const {request} = useHttp();
 
     const updatePassword = (values) => {
-        setSpinner(true);
-        request('http://localhost:8000/sborkaZavodEnergomash/api/update_user.php', 'POST', JSON.stringify(values, null, 2))
-        .then(res => {
-            console.log(res, 'Отправка успешна');
-            setSpinner(false);
-            setSuccesUpdatePasswordMesageState(true);
-            setTimeout(() => {
-                setSuccesUpdatePasswordMesageState(false)
-            }, 10000);
-        })
-        .catch(error => {
-            console.log(error);
-            setSpinner(false);
-            setSuccesUpdatePasswordMesageState(false)
-            setErrorUpdatePassword(true);
-        });
+
+        dispatch(updateUserPass(false));
+
+        // setSpinner(true);
+        // request('http://localhost:8000/sborkaZavodEnergomash/api/update_user.php', 'POST', JSON.stringify(values, null, 2))
+        // .then(res => {
+        //     console.log(res, 'Отправка успешна');
+        //     setSpinner(false);
+        //     setSuccesUpdatePasswordMesageState(true);
+        //     setTimeout(() => {
+        //         setSuccesUpdatePasswordMesageState(false)
+        //     }, 10000);
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        //     setSpinner(false);
+        //     setSuccesUpdatePasswordMesageState(false)
+        //     setErrorUpdatePassword(true);
+        // });
     }
 
     const errorMessage = () => { 
@@ -83,7 +94,7 @@ const UpdatePassword = () => {
                     <input 
                     className='form_submit' 
                     type="submit" 
-                    value='Зарегистрироваться'/>
+                    value='Сменить пароль'/>
                     {errorUpdatePassword ? errorMessage(): null}
                 </div>
                 )
@@ -91,47 +102,52 @@ const UpdatePassword = () => {
     }
 
     return (
-        <Formik
-        initialValues = {{
-            password: '',
-            repeatPassword: ''
-        }}
-        validationSchema = {Yup.object({
-            password: Yup.string('Введите пароль!')
-                    .required('Введите пароль')
-                    .min(7, 'Минимум 7 символов')
-                    .max(255, 'Превышение максимального колличества символов 255'),
-            repeatPassword: Yup.string()
-                    .required('Введите пароль повторно!')
-                    .oneOf([Yup.ref('password')], 'Пароли не совпадают!')
-        })}
-        onSubmit = {(values, {resetForm} ) => {
-            updatePassword(values);
-            resetForm();
-        }}
-        >
-            <Form className='reg_auth_form'>
-                <div className="form_input">
-                    <MyTextInput
-                    label='Пароль'
-                    id="password"
-                    name="password"
-                    type="password"
-                    />
-                </div>
 
-                <div className="form_input">
-                    <MyTextInput
-                    label='Повторите пароль'
-                    id="repeatPassword"
-                    name="repeatPassword"
-                    type="password"
-                    />
-                </div>
-                {submitBtn()}
-                {succesRegMes()}
-            </Form>
-        </Formik>
+        <div className="userRegAuth_body">
+            <div className="userRegAuth_body_choiceRegAuth">
+                <Formik
+                    initialValues = {{
+                     password: '',
+                    repeatPassword: ''
+                    }}
+                    validationSchema = {Yup.object({
+                        password: Yup.string('Введите пароль!')
+                            .required('Введите пароль')
+                            .min(7, 'Минимум 7 символов')
+                            .max(255, 'Превышение максимального колличества символов 255'),
+                        repeatPassword: Yup.string()
+                            .required('Введите пароль повторно!')
+                            .oneOf([Yup.ref('password')], 'Пароли не совпадают!')
+                    })}
+                    onSubmit = {(values, {resetForm} ) => {
+                        updatePassword(values);
+                        resetForm();
+                    }}>
+                        <Form className='reg_auth_form'>
+                            <p>У вас установлен временный пароль. Необходимо его сменить на новый.</p>
+                            <div className="form_input">
+                                <MyTextInput
+                                    label='Новый пароль'
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                />
+                            </div>
+                            <div className="form_input">
+                                <MyTextInput
+                                    label='Повторите пароль'
+                                    id="repeatPassword"
+                                    name="repeatPassword"
+                                    type="password"
+                                />
+                            </div>
+                            {submitBtn()}
+                            {succesRegMes()}
+                        </Form>
+                </Formik>
+            </div>
+        </div>
+        
     )
 }
 
